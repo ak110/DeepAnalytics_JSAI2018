711dc35be0f315e90edfe8c0e324c17609ce1351
aki <mark@aur.ll.to>
2018-02-16 20:57:47 +0900

    20180216_pseudo_labeling

--- Makefile
+++ Makefile
@@ -2,18 +2,29 @@
 
 GPUS?=$(shell python -c 'import pytoolkit as tk ; print(tk.get_gpu_count())')
 # SRC?=$(shell dirname `pwd`)
+BACKUP_DIR=___history/20180216_pseudo_labeling
 
 help:
 	@cat Makefile
 
-run:
+r1:
+	@echo BACKUP_DIR: $(BACKUP_DIR)
+	test ! -e models/model.h5
 	mpirun -np $(GPUS) -H localhost:$(GPUS) python train.py
 	python predict.py --no-cache
+	cp -rv models $(BACKUP_DIR)
 
-predict:
+r2:
+	@echo BACKUP_DIR: $(BACKUP_DIR)
+	test -e models/model.h5
+	mpirun -np $(GPUS) -H localhost:$(GPUS) python train.py --warm --no-validate --pseudo-labeling
+	python predict.py --no-cache
+	cp -rv models $(BACKUP_DIR)
+
+pred:
 	python predict.py --no-cache
 
-valtta:
+val:
 	python predict.py --target=val --no-cache
 
 pip:
--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import pathlib
 import better_exceptions
 import horovod.keras as hvd
 import numpy as np
+import sklearn.externals.joblib as joblib
 
 import data
 import pytoolkit as tk
@@ -28,19 +29,25 @@ def _main():
 def _run():
     import keras
     import models
-
+    logger = tk.log.get(__name__)
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', help='epoch数。', default=300, type=int)
     parser.add_argument('--batch-size', help='バッチサイズ。', default=16, type=int)
-    parser.add_argument('--no-validate', help='バリデーションしない。', action='store_true', default=False)
     parser.add_argument('--warm', help='models/model.h5を読み込む', action='store_true', default=False)
+    parser.add_argument('--no-validate', help='バリデーションしない。', action='store_true', default=False)
+    parser.add_argument('--pseudo-labeling', help='Pseudo-Labelingをする。', action='store_true', default=False)
     args = parser.parse_args()
     validate = not args.no_validate
 
-    (X_train, y_train), (X_val, y_val), _ = data.load_data(split=validate)
+    (X_train, y_train), (X_val, y_val), X_test = data.load_data(split=validate)
     num_classes = len(np.unique(y_train))
     y_train = tk.ml.to_categorical(num_classes)(y_train)
     y_val = tk.ml.to_categorical(num_classes)(y_val) if y_val is not None else None
+    if args.pseudo_labeling:
+        assert not validate
+        X_train = np.concatenate((X_train, X_test))
+        y_train = np.concatenate((y_train, joblib.load(MODELS_DIR / 'pseudo_label.pkl')))
+        logger.info('Pseudo-Labeling: len(X_train) = {}'.format(len(X_train)))
 
     model = models.create_network(num_classes)
 
@@ -53,12 +60,12 @@ def _run():
     model.compile(opt, 'categorical_crossentropy', ['acc'])
 
     if hvd.rank() == 0:
-        model.summary(print_fn=tk.log.get(__name__).info)
-        tk.log.get(__name__).info('network depth: %d', tk.dl.count_network_depth(model))
+        model.summary(print_fn=logger.info)
+        logger.info('network depth: %d', tk.dl.count_network_depth(model))
 
     if args.warm:
         model.load_weights('models/model.h5')
-        tk.log.get(__name__).info('models/model.h5 loaded')
+        logger.info('models/model.h5 loaded')
 
     callbacks = []
     callbacks.append(tk.dl.learning_rate_callback(lr=lr, epochs=args.epochs))
