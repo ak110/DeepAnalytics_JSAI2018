0d81ea605ab619beea51cf18b46e43ef13b7307a
aki <mark@aur.ll.to>
2018-03-11 22:59:29 +0900

    .

--- predict.py
+++ predict.py
@@ -15,7 +15,7 @@ import pytoolkit as tk
 
 _BATCH_SIZE = 48
 _MODELS_DIR = pathlib.Path('models')
-_CACHE_PATH_FORMAT = 'cache/{}/{}/tta{}.pkl'
+_CACHE_NAME_FORMAT = 'pred_{target}_{model}/tta{tta}.pkl'
 _BASE_SIZE = 299
 _SIZE_PATTERNS = [
     (int(_BASE_SIZE * 1.25), int(_BASE_SIZE * 1.25)),
@@ -28,22 +28,22 @@ _SIZE_PATTERNS = [
     (int(_BASE_SIZE * 0.75), int(_BASE_SIZE * 0.75)),
 ]
 _MODELS = [
-    # model_path, cv_index, cv_size, split_seed
-    (_MODELS_DIR / 'model.seed123fold0.h5', 0, 5, 123),
-    (_MODELS_DIR / 'model.seed123fold1.h5', 1, 5, 123),
-    (_MODELS_DIR / 'model.seed123fold2.h5', 2, 5, 123),
-    (_MODELS_DIR / 'model.seed123fold3.h5', 3, 5, 123),
-    (_MODELS_DIR / 'model.seed123fold4.h5', 4, 5, 123),
-    (_MODELS_DIR / 'model.seed234fold0.h5', 0, 5, 234),
-    (_MODELS_DIR / 'model.seed234fold1.h5', 1, 5, 234),
-    (_MODELS_DIR / 'model.seed234fold2.h5', 2, 5, 234),
-    (_MODELS_DIR / 'model.seed234fold3.h5', 3, 5, 234),
-    (_MODELS_DIR / 'model.seed234fold4.h5', 4, 5, 234),
-    (_MODELS_DIR / 'model.seed345fold0.h5', 0, 5, 345),
-    (_MODELS_DIR / 'model.seed345fold1.h5', 1, 5, 345),
-    (_MODELS_DIR / 'model.seed345fold2.h5', 2, 5, 345),
-    (_MODELS_DIR / 'model.seed345fold3.h5', 3, 5, 345),
-    (_MODELS_DIR / 'model.seed345fold4.h5', 4, 5, 345),
+    # models_dir, model_name, cv_index, cv_size, split_seed
+    (_MODELS_DIR / '20180303_InceptionV4_seed123_2', 'model.fold0.h5', 0, 5, 123),
+    (_MODELS_DIR / '20180303_InceptionV4_seed123_2', 'model.fold1.h5', 1, 5, 123),
+    (_MODELS_DIR / '20180303_InceptionV4_seed123_2', 'model.fold2.h5', 2, 5, 123),
+    (_MODELS_DIR / '20180303_InceptionV4_seed123_2', 'model.fold3.h5', 3, 5, 123),
+    (_MODELS_DIR / '20180303_InceptionV4_seed123_2', 'model.fold4.h5', 4, 5, 123),
+    (_MODELS_DIR / '20180303_InceptionV4_seed234_2', 'model.fold0.h5', 0, 5, 234),
+    (_MODELS_DIR / '20180303_InceptionV4_seed234_2', 'model.fold1.h5', 1, 5, 234),
+    (_MODELS_DIR / '20180303_InceptionV4_seed234_2', 'model.fold2.h5', 2, 5, 234),
+    (_MODELS_DIR / '20180303_InceptionV4_seed234_2', 'model.fold3.h5', 3, 5, 234),
+    (_MODELS_DIR / '20180303_InceptionV4_seed234_2', 'model.fold4.h5', 4, 5, 234),
+    (_MODELS_DIR / '20180303_InceptionV4_seed345_2', 'model.fold0.h5', 0, 5, 345),
+    (_MODELS_DIR / '20180303_InceptionV4_seed345_2', 'model.fold1.h5', 1, 5, 345),
+    (_MODELS_DIR / '20180303_InceptionV4_seed345_2', 'model.fold2.h5', 2, 5, 345),
+    (_MODELS_DIR / '20180303_InceptionV4_seed345_2', 'model.fold3.h5', 3, 5, 345),
+    (_MODELS_DIR / '20180303_InceptionV4_seed345_2', 'model.fold4.h5', 4, 5, 345),
 ]
 
 
@@ -66,14 +66,14 @@ def _run():
     args = parser.parse_args()
 
     # 子プロセスを作って予測
-    for model_path, cv_index, cv_size, split_seed in tqdm.tqdm(_MODELS, desc='models', ncols=100, ascii=True):
-        _predict_model(args, model_path, cv_index, cv_size, split_seed)
+    for models_dir, model_name, cv_index, cv_size, split_seed in tqdm.tqdm(_MODELS, desc='models', ncols=100, ascii=True):
+        _predict_model(args, models_dir, model_name, cv_index, cv_size, split_seed)
 
     if args.target == 'test':
         # 集計
-        pred_target_list = [[joblib.load(_CACHE_PATH_FORMAT.format(args.target, model_path.name, tta_index))
+        pred_target_list = [[joblib.load(model_dir / _CACHE_NAME_FORMAT.format(target=args.target, model=model_name, tta=tta_index))
                              for tta_index in range(args.tta_size)]
-                            for model_path, cv_index, _, _ in _MODELS]
+                            for model_dir, model_name, cv_index, _, _ in _MODELS]
         pred_target_proba = np.mean(pred_target_list, axis=(0, 1))
         pred_target = pred_target_proba.argmax(axis=-1)
         # 保存
@@ -81,7 +81,7 @@ def _run():
         data.save_data(_MODELS_DIR / 'submit.tsv', pred_target)
 
 
-def _predict_model(args, model_path, cv_index, cv_size, split_seed):
+def _predict_model(args, models_dir, model_name, cv_index, cv_size, split_seed):
     """GPU数分の子プロセスを作って予測を行う。"""
     with multiprocessing.Manager() as m, concurrent.futures.ProcessPoolExecutor(args.gpus) as pool:
 
@@ -89,34 +89,38 @@ def _predict_model(args, model_path, cv_index, cv_size, split_seed):
         for i in range(args.gpus):
             gpu_queue.put(i)
 
-        futures = [pool.submit(_subprocess, gpu_queue, args, model_path, cv_index, cv_size, split_seed, tta_index)
+        futures = [pool.submit(_subprocess, gpu_queue, args, models_dir, model_name, cv_index, cv_size, split_seed, tta_index)
                    for tta_index in range(args.tta_size)]
-        for f in tqdm.tqdm(futures, desc='tta', ncols=100, ascii=True):
-            f.result()
+        result_set = {f.result() for f in tqdm.tqdm(futures, desc='tta', ncols=100, ascii=True)}
+
+        logger = tk.log.get(__name__)
+        for i, r in enumerate(result_set):
+            logger.debug('subprocess output#{}: {}'.format(i, r))
 
 
 _subprocess_context = {}
 
 
-def _subprocess(gpu_queue, args, model_path, cv_index, cv_size, split_seed, tta_index):
+@tk.capture_output()
+def _subprocess(gpu_queue, args, models_dir, model_name, cv_index, cv_size, split_seed, tta_index):
     """子プロセスの予測処理。"""
     if 'CUDA_VISIBLE_DEVICES' not in os.environ:
-        # GPUの固定 & TensorFlowのログ抑止
+        # GPUの固定
         gpu_id = gpu_queue.get()
         os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_id)
+        # TensorFlowのログ抑止
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
         # モデルの読み込み
         import models
-        _subprocess_context['model'] = models.load(model_path)
+        _subprocess_context['model'] = models.load(models_dir / model_name)
     else:
         import models
 
-    result_path = pathlib.Path(_CACHE_PATH_FORMAT.format(args.target, model_path.name, tta_index))
+    result_path = pathlib.Path(models_dir / _CACHE_NAME_FORMAT.format(target=args.target, model=model_name, tta=tta_index))
     if result_path.is_file():
         print('スキップ: {}'.format(result_path))
+        return
     else:
-        result_path.parent.mkdir(parents=True, exist_ok=True)
-
         seed = 1234 + tta_index
         np.random.seed(seed)
 
@@ -136,6 +140,7 @@ def _subprocess(gpu_queue, args, model_path, cv_index, cv_size, split_seed, tta_
             steps=gen.steps_per_epoch(len(X_target), batch_size),
             verbose=0)
 
+        result_path.parent.mkdir(parents=True, exist_ok=True)
         joblib.dump(proba_target, result_path)
 
 
